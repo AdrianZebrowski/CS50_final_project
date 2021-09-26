e1032fc4263a65ed11aa54774327da1c5d2324d3
Adrian Zebrowski <adrianzebrowski@outlook.com>
2021-09-26 08:38:26 -0400

    wow this is cool

--- snipmenu.py
+++ snipmenu.py
@@ -1,8 +1,52 @@
 import sys
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PIL import ImageGrab
-import numpy as np
-import cv2
+import mss
+import mss.tools
+import pytesseract
+import numpy
+import webbrowser
+
+
+class Snip():
+    def __init__(self, start_point, end_point):
+
+        self.left = min(start_point.x(), end_point.x())
+        self.right = max(start_point.x(), end_point.x())
+        self.top = min(start_point.y(), end_point.y())
+        self.lower = max(start_point.y(), end_point.y())
+        self.bbox = (self.left, self.top, self.right, self.lower)
+
+        with mss.mss() as sct:
+            # TODO: Make this work with multiple monitors
+            # Use the 1st monitor
+            self.monitor = sct.monitors[1]
+            self.im = sct.grab(self.bbox)
+
+    def ocr(self):
+        self.im_rgb = numpy.flip(numpy.array(self.im, dtype=numpy.uint8)[:, :, :3], 2) # BGRA -> RGB conversion
+        self.ocr_text = pytesseract.image_to_string(self.im_rgb)
+        return self
+
+    def search_google(self):
+        url = "https://www.google.com/search?q={}".format(self.ocr_text)
+        webbrowser.open_new_tab(url)
+
+    def search_bing(self):
+        url = "https://www.bing.com/search?q={}".format(self.ocr_text)
+        webbrowser.open_new_tab(url)
+
+    def search_yahoo(self):
+        url = "https://search.yahoo.com/search?p={}".format(self.ocr_text)
+        webbrowser.open_new_tab(url)
+
+    def search_wolfram(self):
+        url = "https://www.wolframalpha.com/input/?i={}".format(self.ocr_text)
+        webbrowser.open_new_tab(url)
+
+    def save(self):
+        mss.tools.to_png(self.im.rgb, self.im.size, output="screenshot.png")
+
 
 class TransparentOverlay(QtWidgets.QWidget):
     def __init__(self):
@@ -10,7 +54,8 @@ class TransparentOverlay(QtWidgets.QWidget):
         QtWidgets.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.CursorShape.CrossCursor))
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-        self.setWindowOpacity(0.5)
+        self.setWindowOpacity(0.1)
+        # TODO: Set this up so it uses the window sizing
         screen_width = 1920
         screen_height = 1200
         self.setGeometry(0, 0, screen_width, screen_height)
@@ -18,30 +63,26 @@ class TransparentOverlay(QtWidgets.QWidget):
         self.start_point = QtCore.QPoint()
 
     def mousePressEvent(self, event):
-        print("Mouse pressed.")
+        # print("Mouse pressed.")
         self.start_point = event.pos()
         self.end_point = event.pos()
         self.update()
-        print(self.end_point.x(), self.end_point.y())
+        # print(self.end_point.x(), self.end_point.y())
 
     def mouseReleaseEvent(self, event):
-        # Call capture function
-        print("Mouse released.")
-        print(self.end_point.x(), self.end_point.y())
+        Snip(self.start_point, self.end_point).ocr().search_wolfram()
+        # Snip(self.start_point, self.end_point).save()
+        # print("Mouse released.")
+        # print(self.end_point.x(), self.end_point.y())
         self.window = MainMenu()
         self.window.show()
         self.close()
         
     def mouseMoveEvent(self, event):
-        print("Mouse moved.")
+        # print("Mouse moved.")
         self.end_point = event.pos()
         self.update()
-        print(self.end_point.x(), self.end_point.y())
-
-class Snip(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        print("Snip initialized...")
+        # print(self.end_point.x(), self.end_point.y())
 
 
 class MainMenu(QtWidgets.QWidget):
